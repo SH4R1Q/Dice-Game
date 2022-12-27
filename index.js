d136711b19021a2510fa18b40b943eaa8b26af08
Shariq Khan <94235303+SH4R1Q@users.noreply.github.com>
2022-12-27 20:09:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function winner(name, num){
-    document.querySelector(name+" img").setAttribute("src" , "/images/dice"+num+".png");
+    document.querySelector(name+" img").setAttribute("src" , "images/dice"+num+".png");
     document.querySelector(name+" p").textContent = "Winner!! 🚩";
 }
 
 function loser(name, num){
-    document.querySelector(name+" img").setAttribute("src" , "/images/dice"+num+".png");
+    document.querySelector(name+" img").setAttribute("src" , "images/dice"+num+".png");
     document.querySelector(name+" p").textContent = "Lost 😶";
 }
 function draw(name1, num1, name2, num2){
-    document.querySelector(name1+" img").setAttribute("src" , "/images/dice-roll.gif");
-    document.querySelector(name2+" img").setAttribute("src" , "/images/dice-roll.gif");
-    document.querySelector(name1+" img").setAttribute("src" , "/images/dice"+num1+".png");
-    document.querySelector(name2+" img").setAttribute("src" , "/images/dice"+num2+".png");
+    document.querySelector(name1+" img").setAttribute("src" , "images/dice-roll.gif");
+    document.querySelector(name2+" img").setAttribute("src" , "images/dice-roll.gif");
+    document.querySelector(name1+" img").setAttribute("src" , "images/dice"+num1+".png");
+    document.querySelector(name2+" img").setAttribute("src" , "images/dice"+num2+".png");
     document.querySelector(name1+" p").textContent = "Draw 🙌";
     document.querySelector(name2+" p").textContent = "Draw 🙌";
 }
