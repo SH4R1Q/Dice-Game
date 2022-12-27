e89d2d7a24699fd9b281423d75124e4e94a74bb1
Shariq Khan <94235303+SH4R1Q@users.noreply.github.com>
2022-12-27 23:57:14 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function loser(name, num){
     document.querySelector(name+" p").textContent = st+" : Lost 😶";
 }
 function draw(name1, num1, name2, num2){
-    document.querySelector(name1+" img").setAttribute("src" , "images/dice-roll.gif");
-    document.querySelector(name2+" img").setAttribute("src" , "images/dice-roll.gif");
+    // document.querySelector(name1+" img").setAttribute("src" , "images/dice-roll.gif");
+    // document.querySelector(name2+" img").setAttribute("src" , "images/dice-roll.gif");
     document.querySelector(name1+" img").setAttribute("src" , "images/dice"+num1+".png");
     document.querySelector(name2+" img").setAttribute("src" , "images/dice"+num2+".png");
     document.querySelector(name1+" p").textContent = "Draw 🙌";
@@ -19,18 +19,22 @@ function draw(name1, num1, name2, num2){
 }
 
 function gen() {
+    document.querySelector("#player1 p").textContent = "Player 1";
+    document.querySelector("#player2 p").textContent = "Player 2";
+    document.querySelector("#player1 img").setAttribute("src" , "images/dice-roll.gif");
+    document.querySelector("#player2 img").setAttribute("src" , "images/dice-roll.gif");
     let n1 = Math.floor(Math.random()*6)+1;
     let n2 = Math.floor(Math.random()*6)+1;
     if(n1<n2) {
-        winner("#player2", n2);
-        loser("#player1" , n1);
+        setTimeout(winner,1500,"#player2", n2);
+        setTimeout(loser,1500,"#player1", n1);
     }
     if(n2<n1) {
-        winner("#player1", n1);
-        loser("#player2" , n2);
+        setTimeout(winner,1500,"#player1", n1);
+        setTimeout(loser,1500,"#player2", n2);
     }
     if(n1 === n2){
-        draw("#player1", n1, "#player2", n2);
+        setTimeout(draw,1500,"#player1", n1, "#player2", n2);
     }
 }
 document.getElementById("announce").addEventListener("click", gen);
